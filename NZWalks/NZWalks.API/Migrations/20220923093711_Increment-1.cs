a6d0d546642de6868ddca43283e70e00ff0cfa47
sourangsu7 <sourangsu.d@outlook.com>
2022-09-23 15:14:30 +0530

    Added domain data objects and EFCore injection , along with initial round of migration

---
+++ NZWalks/NZWalks.API/Migrations/20220923093711_Increment-1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    public partial class Increment1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Area",
+                table: "Regions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "Regions");
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,15 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Area { get; set; }
+        public long Population { get; set; }
+        //navigational properties
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
